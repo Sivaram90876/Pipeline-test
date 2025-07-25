dbaa8ac2e66708194c2fcba70349b855d1aaa478
Sivaram90876 <siva.routhu@track3d.ai>
2025-07-25 16:52:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello from your Jenkins CI/CD Node.js app!\n');
+});
+
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}/`);
+});
